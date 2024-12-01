3e00291c555c733242b9e0dd8a9019daeea5b291
Ignacio Sánchez <ignacio.sanchez@euroinnova.com>
2024-12-01 07:52:18 +0100

    ongoing... add string representation to schemas

--- llm/utils.py
+++ llm/utils.py
@@ -1,9 +1,24 @@
+from functools import partial
+from db import Session
 from db.crud import (
     get_stylist, 
+    get_all_stylists,
     get_service, 
     get_customer, 
     get_appointment, 
     get_work_schedule
 )
+import json
 
-def create_context():
+
+def get_stylists_data():
+    return get_all_stylists()
+
+if __name__ == "__main__":
+    db = Session()
+    stylists = get_all_stylists(db)
+    #data = json.dumps([dict(stylist) for stylist in stylists], default=str)
+    print(str(stylists))
+
+
+    
